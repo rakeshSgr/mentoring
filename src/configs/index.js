8e7648e6ee4c0779cb8c0aab198fdcf2181b784a
nevil <nevil@tunerlabs.com>
2024-06-11 16:04:28 +0530

    deleted: config schema validation

--- src/configs/index.js
+++ src/configs/index.js
@@ -5,9 +5,6 @@
  * Description : Contains connections of all configs
  */
 
-const Ajv = require('ajv')
-const config = require('./search.json')
-
 require('./kafka')()
 
 require('./cache')()
@@ -16,84 +13,3 @@ require('./bull')()
 
 const path = require('path')
 global.PROJECT_ROOT_DIRECTORY = path.join(__dirname, '..')
-;(function () {
-	const schema = {
-		title: 'Generated schema for Root',
-		type: 'object',
-		properties: {
-			search: {
-				type: 'object',
-				properties: {
-					sessionSearch: {
-						type: 'object',
-						properties: {
-							fields: {
-								type: 'array',
-								items: {
-									type: 'object',
-									properties: {
-										name: {
-											type: 'string',
-										},
-										label: {
-											type: 'string',
-										},
-										sortPriority: {
-											type: 'number',
-										},
-										isAnEntityType: {
-											type: 'boolean',
-										},
-									},
-									required: ['name', 'label', 'sortPriority'],
-								},
-							},
-						},
-						required: ['fields'],
-					},
-					mentorSearch: {
-						type: 'object',
-						properties: {
-							fields: {
-								type: 'array',
-								items: {
-									type: 'object',
-									properties: {
-										name: {
-											type: 'string',
-										},
-										label: {
-											type: 'string',
-										},
-										sortPriority: {
-											type: 'number',
-										},
-										isAnEntityType: {
-											type: 'boolean',
-										},
-									},
-									required: ['name', 'label', 'sortPriority'],
-								},
-							},
-						},
-						required: ['fields'],
-					},
-				},
-				required: ['sessionSearch', 'mentorSearch'],
-			},
-		},
-		required: ['search'],
-	}
-
-	const ajv = new Ajv()
-
-	const validate = ajv.compile(schema)
-	const valid = validate(config)
-
-	if (!valid) {
-		console.error('Invalid Search config:', validate.errors)
-		process.exit(1)
-	} else {
-		console.log('Config is valid.')
-	}
-})()
