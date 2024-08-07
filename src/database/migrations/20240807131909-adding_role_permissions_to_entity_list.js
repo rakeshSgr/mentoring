4f6af895ce72802bb48a957b5e1833f7a26ada29
sumanvpacewisdom <suman.v@pacewisdom.com>
2024-08-07 19:02:43 +0530

    Adding Permission,Role Permission for Enitity List

---
+++ src/database/migrations/20240807131909-adding_role_permissions_to_entity_list.js
@@ -0,0 +1,67 @@
+'use strict'
+
+require('module-alias/register')
+const userRequests = require('@requests/user')
+require('dotenv').config()
+const common = require('@constants/common')
+const Permissions = require('@database/models/index').Permission
+
+const getPermissionId = async (module, request_type, api_path) => {
+	try {
+		const permission = await Permissions.findOne({
+			where: { module, request_type, api_path },
+		})
+		if (!permission) {
+			throw error
+		}
+		return permission.id
+	} catch (error) {
+		throw error
+	}
+}
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		try {
+			const rolePermissionsData = [
+				{
+					role_title: common.MENTEE_ROLE,
+					permission_id: await getPermissionId('entity', ['POST'], '/mentoring/v1/entity/list'),
+					module: 'entity',
+					request_type: ['POST'],
+					api_path: '/mentoring/v1/entity/list',
+					created_at: new Date(),
+					updated_at: new Date(),
+					created_by: 0,
+				},
+				{
+					role_title: common.MENTOR_ROLE,
+					permission_id: await getPermissionId('entity', ['POST'], '/mentoring/v1/entity/list'),
+					module: 'entity',
+					request_type: ['POST'],
+					api_path: '/mentoring/v1/entity/list',
+					created_at: new Date(),
+					updated_at: new Date(),
+					created_by: 0,
+				},
+				{
+					role_title: common.SESSION_MANAGER_ROLE,
+					permission_id: await getPermissionId('entity', ['POST'], '/mentoring/v1/entity/list'),
+					module: 'entity',
+					request_type: ['POST'],
+					api_path: '/mentoring/v1/entity/list',
+					created_at: new Date(),
+					updated_at: new Date(),
+					created_by: 0,
+				},
+			]
+			await queryInterface.bulkInsert('role_permission_mapping', rolePermissionsData)
+		} catch (error) {
+			console.error(error)
+		}
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkDelete('role_permission_mapping', null, {})
+	},
+}
