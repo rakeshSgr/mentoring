ce862c98f07df12a2ecc5222d896e7d3cdbc2fed
sumanvpacewisdom <suman.v@pacewisdom.com>
2024-08-07 21:35:36 +0530

    const to function

--- src/generics/utils.js
+++ src/generics/utils.js
@@ -745,7 +745,7 @@ function validateProfileData(profileData, validationData) {
 	return profileMandatoryFields
 }
 
-const convertExpiryTimeToSeconds = (expiryTime) => {
+function convertExpiryTimeToSeconds(expiryTime) {
 	const match = expiryTime.match(/^(\d+)([m]?)$/)
 	if (match) {
 		const value = parseInt(match[1], 10) // Numeric value
