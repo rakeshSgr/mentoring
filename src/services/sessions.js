2b343afd9c2ba2986eb83d754d0389b45a8dcff4
sumanvpacewisdom <suman.v@pacewisdom.com>
2024-08-09 16:06:56 +0530

    Fix : Comment changes

--- src/services/sessions.js
+++ src/services/sessions.js
@@ -2209,17 +2209,17 @@ module.exports = class SessionsHelper {
 	 * Bulk update mentor names for sessions.
 	 * @method
 	 * @name bulkUpdateMentorNames
-	 * @param {Array} mentorsId - Array of mentor IDs to update.
+	 * @param {Array} mentorsIds - Array of mentor IDs to update.
 	 * @param {STRING} mentorsName - Mentor name that needs to be updated.
 	 * @returns {Object} - Success response indicating the update was performed successfully.
 	 * @throws {Error} - Throws an error if there's an issue during the bulk update.
 	 */
-	static async bulkUpdateMentorNames(mentorsId, mentorsName) {
+	static async bulkUpdateMentorNames(mentorsIds, mentorsName) {
 		try {
-			mentorsId = mentorsId.map(String)
+			mentorsIds = mentorsIds.map(String)
 			await sessionQueries.updateSession(
 				{
-					mentor_id: mentorsId,
+					mentor_id: mentorsIds,
 				},
 				{
 					mentor_name: mentorsName,
